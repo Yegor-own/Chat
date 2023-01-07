90b9a4bb57e3e466a6827d202cfbe589aefab131
Yegor-own <egor891712@gmail.com>
2023-01-07 21:05:19 +0500

    feat: add repository and service for relationship

--- src/v1/pkg/usecase/repository/relationship_repository.go
+++ src/v1/pkg/usecase/repository/relationship_repository.go
@@ -1,1 +1,59 @@
 package repository
+
+import (
+	"github.com/Yegor-own/Chat/src/v1/pkg/entities"
+	"gorm.io/gorm"
+)
+
+type RelationshipRepository interface {
+	CreateRelationship(uId, cId uint) (*entities.Relationship, error)
+	ReadByUserId(id uint) (*entities.Relationship, error)
+	ReadByChatId(id uint) (*entities.Relationship, error)
+	DeleteRelationship(relationshipId uint) error
+}
+
+func NewRelationshipRepositorySQL(db *gorm.DB) RelationshipRepository {
+	return &relationshipRepository{
+		db: db,
+	}
+}
+
+func (r *relationshipRepository) CreateRelationship(uId, cId uint) (*entities.Relationship, error) {
+	rel := entities.Relationship{
+		UserID: uId,
+		ChatID: cId,
+	}
+
+	res := r.db.Create(&rel)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return &rel, nil
+}
+
+func (r *relationshipRepository) ReadByUserId(id uint) (*entities.Relationship, error) {
+	rel := entities.Relationship{}
+	res := r.db.First(&rel, "UserID = ?", id)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return &rel, nil
+}
+
+func (r *relationshipRepository) ReadByChatId(id uint) (*entities.Relationship, error) {
+	rel := entities.Relationship{}
+	res := r.db.First(&rel, "ChatID = ?", id)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return &rel, nil
+}
+
+func (r *relationshipRepository) DeleteRelationship(relationshipId uint) error {
+	rel := entities.Relationship{ID: relationshipId}
+	res := r.db.Delete(&rel)
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
+}
--- src/v1/pkg/usecase/service/relationship_service.go
+++ src/v1/pkg/usecase/service/relationship_service.go
@@ -1,1 +1,39 @@
 package service
+
+import (
+	"github.com/Yegor-own/Chat/src/v1/pkg/entities"
+	"github.com/Yegor-own/Chat/src/v1/pkg/usecase/repository"
+)
+
+type RelationshipService interface {
+	InsertRelationship(uId, cId uint) (*entities.Relationship, error)
+	GetRelationshipByUserId(id uint) (*entities.Relationship, error)
+	GetRelationshipByChatId(id uint) (*entities.Relationship, error)
+	RemoveRelationship(relationshipId uint) error
+}
+
+type relationshipService struct {
+	repository repository.RelationshipRepository
+}
+
+func NewRelationshipService(r repository.RelationshipRepository) RelationshipService {
+	return &relationshipService{
+		repository: r,
+	}
+}
+
+func (s *relationshipService) InsertRelationship(uId, cId uint) (*entities.Relationship, error) {
+	return s.repository.CreateRelationship(uId, cId)
+}
+
+func (s *relationshipService) GetRelationshipByUserId(id uint) (*entities.Relationship, error) {
+	return s.repository.ReadByUserId(id)
+}
+
+func (s *relationshipService) GetRelationshipByChatId(id uint) (*entities.Relationship, error) {
+	return s.repository.ReadByChatId(id)
+}
+
+func (s *relationshipService) RemoveRelationship(relationshipId uint) error {
+	return s.repository.DeleteRelationship(relationshipId)
+}
